db44f10d0b8b2cecf68d49de9c43cef3d895e867
tako2039 <takeo2039@gmail.com>
2022-04-10 14:57:18 -0500

    check difference between value and reference

--- Main.java
+++ Main.java
@@ -1,8 +1,35 @@
 package com.takeo;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	int myIntValue = 10;
+    int anotherIntValue = myIntValue;
+
+        System.out.println("myIntValue: " + myIntValue);
+        System.out.println("anotherIntValue: " + anotherIntValue);
+
+        anotherIntValue++;
+
+        System.out.println("myIntValue: " + myIntValue);
+        System.out.println("anotherIntValue: " + anotherIntValue);
+
+        int[] myIntArray = new int[5];
+        for (int i=0; i<myIntArray.length; i++) {
+            myIntArray[i] = i;
+        }
+        int[] anotherIntArray = myIntArray;
+
+        System.out.println("myIntValue: " + Arrays.toString(myIntArray));
+        System.out.println("anotherIntValue: " + Arrays.toString(anotherIntArray));
+
+        for (int i=0; i<anotherIntArray.length; i++) {
+            anotherIntArray[i] = i + 1;
+        }
+
+        System.out.println("myIntValue: " + Arrays.toString(myIntArray));
+        System.out.println("anotherIntValue: " + Arrays.toString(anotherIntArray));
     }
 }
